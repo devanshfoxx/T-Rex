23c7439c1f8598e3a5f2f337ac8bbc18d9c030c9
devanshfoxx <89197169+devanshfoxx@users.noreply.github.com>
2021-08-23 16:11:51 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -9,13 +9,20 @@ function setup() {
 redb = createButton ("red");
 redb.position (100,50);
 redb.mousePressed (red_bg);
+greenb = createButton ("green");
+greenb.position (200,50);
+greenb.mousePressed (green_bg);
 }
 function red_bg () {
 r = 255; 
 g = 0;
 b = 0;
 }
-
+function green_bg () {
+r = 0;
+g = 255;
+b = 0;
+}
 function draw() 
 {
   background(r,g,b);
